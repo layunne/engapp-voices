4d256ec1893602551e5dcef7104ea2a40a2407da
Fernando Calixto <fernandocalixtodev@gmail.com>
2018-11-26 22:52:45 -0300

    Rollback in the Primary Color Blue

--- src/assets/jss/AudioCard.js
+++ src/assets/jss/AudioCard.js
@@ -3,16 +3,16 @@ export default theme => ({
 
     eachCard: {
         /*background: 'linear-gradient(to right, #02499b 80%, #fff)',*/
-        border: '1px solid #02499b',
+        /*border: '1px solid #02499b',*/
     },
 
     titBoxes: {
         textAlign: 'center',
-        color: '#02499b',
+
     },
 
     btnControls: {
-        color: '#02499b',
+
     },
 
 });
