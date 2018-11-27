f52ac947d3b3d354f84f60372d3ef6fe2c1704c7
Fernando Calixto <fernando@super.cash>
2018-11-26 22:23:04 -0300

    Starting JSS Style Page

---
+++ src/assets/jss/AudioCard.js
@@ -0,0 +1,12 @@
+
+export default theme => ({
+
+    eachCard: {
+        /*background: 'linear-gradient(to right, #03132a, #fff)',*/
+    },
+
+    titBoxes: {
+        textAlign: 'center',
+    },
+
+});
