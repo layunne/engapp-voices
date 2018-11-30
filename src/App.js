5c733c75e1c1998038a65c947e51ff762e5a3d38
JandersonCRB <jandersonangelo@hotmail.com>
2018-11-30 18:21:08 -0300

    add audios

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ import 'typeface-roboto';
 
 const audios = [
 	{ title: 'Hassum meu irmão, e aí?', audio: require('./audio.ogg') },
+	{ title: 'Eu tô chorando porra', audio: require('./audio2.ogg') },
+	{ title: 'Fdp não sabe digitar', audio: require('./audio3.ogg') },
+	{ title: 'Bolsonaro zoio de lula', audio: require('./audio4.ogg') },
+	{ title: 'CTO Rino', audio: require('./audio5.ogg') },
+	{ title: 'Se eu tivesse uma arma eu atirava no primeiro de azul', audio: require('./audio6.ogg') },
+	// { title: 'Aqui quem fala é Marco Barbosa', audio: require('./audio7.aac') },
+	{ title: 'Bolsonaro com tuberculose', audio: require('./audio8.ogg') },
+	{ title: 'Tome no cu esse csa', audio: require('./audio9.ogg') },
+	{ title: 'Isso aqui tá uma porra', audio: require('./audio10.ogg') },
+	{ title: 'Hugo saia do crossfit', audio: require('./audio11.ogg') },
 ]
 
 class App extends Component {
