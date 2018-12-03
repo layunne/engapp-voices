4c45f992054913d2f2627c0b596f447d49837c20
Fernando Calixto <fernandocalixtodev@gmail.com>
2018-12-02 21:44:29 -0300

    Fixing Card Size and Controls Align

--- src/assets/jss/AudioCard.js
+++ src/assets/jss/AudioCard.js
@@ -4,10 +4,17 @@ export default theme => ({
     eachCard: {
         /*background: 'linear-gradient(to right, #02499b 80%, #fff)',*/
         /*border: '1px solid #02499b',*/
+        height: 370,
     },
 
     titBoxes: {
         textAlign: 'center',
+        height: 195, 
+
+        [theme.breakpoints.down('xs')]:{
+            height: 180,
+        
+        }
 
     },
 
